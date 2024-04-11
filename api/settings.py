24c9a64a0139361615381fe3a803378ba35f5b87
Vinicius Paiva <vinicius.arantespaiva@gmail.com>
2024-04-11 01:35:50 -0300

    Criando model para Produtor

--- api/settings.py
+++ api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*e&pbl5lai4&1=2h2s7%_5$51^@i^3y^1i+$-75e(vtx%y0ufq'
+SECRET_KEY = '123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- rural/models.py
+++ rural/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from rural.utils import STATE_CHOICES
+
+
+class Producer(models.Model):
+    cpf_cnpj = models.CharField(max_length=18)
+    name = models.CharField(max_length=100)
+    farm_name = models.CharField(max_length=100)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES)
+    city = models.CharField(max_length=100)
+    farm_area_ha = models.DecimalField(max_digits=8, decimal_places=2)
+    agro_area_ha = models.DecimalField(max_digits=8, decimal_places=2)
+    veg_area_ha = models.DecimalField(max_digits=8, decimal_places=2)
+    planting = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
